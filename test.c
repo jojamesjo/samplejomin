0c44f44eb95b9ca28e4bf8eeafb07cee49e09afe
jojamesjo <jomin.james@vvdntech.in>
2019-08-28 17:49:58 +0530

    Z

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio>
+void main()
+{
+int a,b,c,d;
+printf("enter first the number\n");
+scanf("%d",&a);
+printf("enter second the number\n");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+printf("result is %d\n",c);
+printf("result is %d\n",d);
+return 0;
+
+}
+
