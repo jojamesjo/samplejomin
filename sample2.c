3d1a415cc104b5c756c4e9bf0d22c0d3a41a75a9
jojamesjo <jomin.james@vvdntech.in>
2019-08-30 15:14:25 +0530

    new sample prng

---
+++ sample2.c
@@ -0,0 +1,17 @@
+#include<stdio>
+void main()
+{
+int a,b,c,d;
+printf("enter first the number\n");
+scanf("%d",&a);
+printf("enter second the number\n");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+printf("result is %d\n",c);
+printf("result is %d\n",d);
+printF("hello friend";
+printf("hello hello hai");
+return 0;
+
+}
